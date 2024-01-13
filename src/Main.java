cd8993a1c7c6e4269d1431ae1956c42b0e32e3b1
Olga Belova <lera2205@mail.ru>
2024-01-13 21:54:04 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int ost = 100;
+        int amount = 1200;
+        int bonus = amount / 100;
+        int sum;
+        if (amount >= 1000) {
+            sum = bonus + amount + ost;
+        } else {
+            sum = amount + ost;
         }
+        System.out.println("Итого на счете:" + sum);
+        System.out.println("Начислено бонусов:" + bonus);
     }
+}
 }
